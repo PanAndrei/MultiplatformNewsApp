9abd4837066792454d48de185a1c9a9d7ffbb8a1
Andrei Panasenko <andrei_kmv@inbox.ru>
2023-03-27 18:43:29 +0300

    Initial Commit

---
+++ MultiplatformNewsApp/MultiplatformNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplatformNewsAppApp.swift
+//  MultiplatformNewsApp
+//
+//  Created by Andrei Panasenko on 27.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MultiplatformNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
